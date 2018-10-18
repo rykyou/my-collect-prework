93c151fb0285ca02a061254ec22b01d430324029
Robin Kim <rkim1214@gmail.com>
2018-10-18 03:32:52 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  i = 0 
+  new_array = []
+  
+  while i < array.length 
+    new_element = yield array[i]
+    new_array << new_element
+    i += 1 
+  end 
+  
+  new_array
+end 
